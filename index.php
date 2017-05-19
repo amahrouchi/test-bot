8240bb019984e3f79869f557593ff2ac8a69004c
Angelo Mahrouchi[user] <angelo.mahrouchi@gmail.com>
2017-05-19 17:50:41 +0200

    echo code

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 echo $_GET['hub_mode'];
+echo $_GET['hub_verify_token'];
+echo $_GET['hub_challenge'];
 
 if ($_GET['hub']['mode'] === 'subscribe' && $_GET['hub']['verify_token'] === 'my_token_is_awesome')
 {
